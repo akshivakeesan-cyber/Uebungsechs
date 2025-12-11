a881c582ac1de5bd00e075aeb6a54121feba0e07
Akshitha Vakeesan <akshithavakeesan@MacBook-Air-von-Akshitha.local>
2025-12-11 11:37:12 +0100

    erledigt

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -1,5 +1,30 @@
 package h2;
 
 public class H2_main {
+	public static void main (String []args) {
+
+		int [] a = {1,2,3};
+		int [] b = {1,2,8};
+		
+		System.out.println(compareArrays(a,b));
+	}
+	
+public static boolean compareArrays ( int a [], int b[])	
+{
+	if (a.length != b.length)
+	{
+		return false;
+	}
+	
+	for (int i =0; i<b.length; i++)
+	{
+		if ( a[i] != b[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
 
 }
+
